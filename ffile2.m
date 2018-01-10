d2a4bd982a84c43a4bfeb706cf3bfa9b25363439
bill <wsb@soton.ac.uk>
2018-01-10 11:08:31 +0000

    first change
    changed a bit of ffile2.m

---
+++ ffile2.m
@@ -0,0 +1,10 @@
+%this is the second file
+
+x = 1
+y = 2
+
+Foo = x + y
+
+% changing this file locally
+
+fprintf('changed\n')
---
+++ file1.m
@@ -0,0 +1,8 @@
+%test stuff
+
+
+Clear all
+x = 5
+y = 6
+
+plot(x,y)
